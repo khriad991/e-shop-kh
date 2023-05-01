17aef932e47c9eb869bd3ae412937f91c7a2cf4b
Kamrul Hasan <991khriad@gmail.com>
2023-05-01 13:51:46 +0600

    Login Page layout is done

---
+++ frontend/src/pages/LoginPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Login from "../componets/login/Login";
+
+const LoginPage = () => {
+
+    return (
+        <div>
+            <Login/>
+        </div>
+    );
+};
+
+export default LoginPage;
