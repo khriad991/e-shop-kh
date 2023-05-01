95aad11f551ef1329a1f9aa7f1ba11cfa94834a6
Kamrul Hasan <991khriad@gmail.com>
2023-05-01 16:25:39 +0600

    singUp page layout is done

---
+++ frontend/src/pages/SingUpPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import SingUp from "../componets/singup/SingUp";
+
+const SingUpPage = () => {
+
+    return (
+        <div>
+            <SingUp/>
+        </div>
+    );
+};
+
+export default SingUpPage;
