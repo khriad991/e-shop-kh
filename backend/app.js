65382fbaa93052c12f58c1d40a1fb533c4767879
Kamrul Hasan <63355519+khriad991@users.noreply.github.com>
2023-05-04 00:12:14 +0600

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ const bodyParser = require("body-parser");
 app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended: true}))
-// app.use(fileUPload({useTempFiles: true}))
+// app.use(fileUPload({useTempFiles: true})) // comment for simple errorn this package 
 
 // .env file import ----->>
 if (process.env.NODE_ENV !== "PRODUCTION") {
